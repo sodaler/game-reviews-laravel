28dc2e9491cd221da2b5fcc70ffadd17e7438f67
sodaler <misha_lector87@mail.ru>
2022-07-06 17:36:12 +0400

    add game template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController')->name('main.index');
 });
+
+Auth::routes();
+
